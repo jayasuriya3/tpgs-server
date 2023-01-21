e42a0e6d51fc7eaba5d15207234012ff01b59d6b
Faisal Mahamud <97041463+FaisalMahamudCS@users.noreply.github.com>
2023-01-21 13:42:52 +0600

    updated Model.js connection query

--- model/model.js
+++ model/model.js
@@ -2,10 +2,12 @@ const env = process.env.NODE_ENV || 'development';
 const dbConfig = require(__dirname + '/../config/config.json')[env];
 
 const Sequelize = require("sequelize");
-const sequelize = new Sequelize(dbConfig.database, dbConfig.username, dbConfig.password, {
-  host: dbConfig.host,
-  dialect: dbConfig.dialect
-});
+// const sequelize = new Sequelize(dbConfig.database, dbConfig.username, dbConfig.password, {
+//   host: dbConfig.host,
+//   dialect: dbConfig.dialect
+// });
+const sequelize = new Sequelize(dbConfig.database, dbConfig.username, dbConfig.password, dbConfig);
+
 
 const db = {};
 
