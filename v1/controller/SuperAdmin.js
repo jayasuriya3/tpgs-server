ab1fc300ee7a6e8aa291dd1fc2631701a146a470
Faisal Mahamud <97041463+FaisalMahamudCS@users.noreply.github.com>
2023-02-01 17:05:21 +0600

    updated super admin

--- v1/controller/SuperAdmin.js
+++ v1/controller/SuperAdmin.js
@@ -1,4 +1,4 @@
-//const Model = require("../../model/index");
+//const Model = require("../../model/index");   
 const User = require("../../models").User;
 const Service = require("../../models").Service;
 const Accessory = require("../../models").Accessory;
